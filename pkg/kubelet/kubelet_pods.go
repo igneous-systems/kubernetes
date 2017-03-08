922af83747ab3466cc7327f724d9c52c2ade6b6b
andrewsykim <andrew@wattpad.com>
2017-03-08 03:08:09 -0500

    support hostIP in downward API
    (cherry picked from commit 91c027d6cccce36a6b1116313142f45884f8a7b0)

--- pkg/kubelet/kubelet_pods.go
+++ pkg/kubelet/kubelet_pods.go
@@ -643,6 +643,8 @@ func (kl *Kubelet) podFieldSelectorRuntimeValue(fs *v1.ObjectFieldSelector, pod 
 		return pod.Spec.NodeName, nil
 	case "spec.serviceAccountName":
 		return pod.Spec.ServiceAccountName, nil
+	case "status.hostIP":
+		return pod.Status.HostIP, nil
 	case "status.podIP":
 		return podIP, nil
 	}
